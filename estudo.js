14b1a0b6ce101ac4e8c94b794514c7b38dc6485e
Charopinplay <110104504+Charopinplay@users.noreply.github.com>
2022-10-05 09:58:41 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,4 +1,4 @@
-let tabuada = 2;
+let tabuada = 0;
 
 function escreva(){
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
@@ -13,4 +13,17 @@ function escreva(){
   document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
   
 
+}
+function minhaTabuada(){
+   for(let i = 1; i <= 11; i++i){
+     document.write(Tabuada do " i + "<br>");
+    for(let j = 1; j <= 10; j++i){
+   document.write(tabuada + " x " + i + " = " + tabuada*i) "<br>");
+    }
+document.write("<br>");
+  }
+}
+function cubo(){
+  for(let i = 20; i >= 2 ; i--){
+    Document.write(" o cubo de " + i + "(+ i "x"+ "x" + i + "=" " "=(i*i*i") + " <br>);
 }
